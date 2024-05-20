8bd596b85e8840abb571c6247cf9c02cd183eb09
Vinicius Guerra <vini.fms@hotmail.com>
2024-05-20 14:11:44 -0300

    Tsyringe routes

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { container } from "tsyringe";
+import { CarServices } from "../services/car.services";
+import { CarControllers } from "../controllers/car.controllers";
 
 export const carRouter = Router();
 
-carRouter.post("/");
-carRouter.get("/");
-carRouter.get("/:id");
-carRouter.patch("/:id");
-carRouter.delete("/:id");
+container.registerSingleton("CarServices", CarServices);
+const carControllers = container.resolve(CarControllers);
+
+carRouter.post("/", (req, res) => carControllers.create(req,res));
+carRouter.get("/", (req, res) => carControllers.getMany(req,res));
+carRouter.get("/:id", (req, res) => carControllers.getOne(req,res));
+carRouter.patch("/:id", (req, res) => carControllers.update(req,res));
+carRouter.delete("/:id", (req, res) => carControllers.delete(req,res));
